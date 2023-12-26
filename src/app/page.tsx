773da802fd0250fce74e2997c119e821c896b741
Lazar Stojanović <lakisd98@gmail.com>
2023-12-26 06:50:24 +0100

    Add: card hover effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,20 @@ import Profile from "../../public/image-avatar.webp";
 
 export default function Home() {
 	return (
-		<main>
-			<div className="bg-card-white p-4 m-6 rounded-xl flex flex-col gap-4 border border-card-black card">
+		<main className="max-w-sm">
+			<div className="bg-card-white p-6 m-6 rounded-xl flex flex-col gap-4 border border-card-black card">
 				<Image
 					src={Article}
 					alt="article illustration"
-					className="rounded-xl h-[200px] object-cover"
+					className="rounded-xl h-[200px] object-cover self-center"
 				/>
 				<div className="bg-card-yellow px-2 py-1 rounded-md w-[30%] text-center">
 					<p className="font-extrabold text-sm">Learning</p>
 				</div>
 				<p className="font-semibold text-sm">Published 21 Dec 2023</p>
-				<h1 className="text-xl font-extrabold">HTML & CSS foundations</h1>
+				<h1 className="text-xl font-extrabold cursor-pointer hover:text-card-yellow">
+					HTML & CSS foundations
+				</h1>
 				<p className="text-card-gray tracking-tight">
 					These languages are the backbone of every website, defining structure, content,
 					and presentation
